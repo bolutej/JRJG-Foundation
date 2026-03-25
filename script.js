878a934fa872eae293af48aec0bb4d34f7f0852f
Bolu Tejumola <tejumolabolu@gmail.com>
2026-03-25 23:53:32 +0100

    contact form

--- script.js
+++ script.js
@@ -13,11 +13,42 @@ document.querySelectorAll(".sides a").forEach((link) => {
     sides.classList.remove("open");
   });
 });
+function showToast(message, type ="success") {
+  const toast = document.getElementById("toast");
+  toast.textContent = message;
+  toast.className = `toast ${type} show`;
 
-// const images = document.querySelectorAll(".slides-track img");
+  setTimeout(() => {
+    toast.className = "toast";
+  }, 3000);
+}
+async function sendMessage(e) {
+  e.preventDefault();
 
-// images.forEach(img => {
-//     const max = 10;
-//     const deg = (Math.random() * max * 2) - max;
-//     img.style.transform = `rotate(${deg}deg)`;
-// })
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+try {
+  const res = await fetch(
+    "https://byqopxrcjrqdofhlwkma.supabase.co/functions/v1/send-contact-email",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify({ name, email, message}),
+    }
+  );
+
+  const data = await res.json();
+
+  if (data.success) {
+    showToast("✅ Message sent successfully!", "success");
+    e.target.reset();
+  } else {
+    showToast("❌ Something went wrong. Try again.", "error");
+  }
+ } catch (err) {
+    showToast("❌ Network error. Try again.", "error");
+  }
+}
+
+document.getElementById("form").addEventListener("submit", sendMessage);
